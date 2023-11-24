17a788b40df1e02f5759f3da2db637328f9f1223
Bill Sellers <wis@mac.com>
2023-11-24 15:20:15 +0000

    Moved around some files and made PhysXQt console only

--- PhysX/PhysXQt/main.cpp
+++ PhysX/PhysXQt/main.cpp
@@ -1,11 +1,6 @@
-#include "mainwindow.h"
-
-#include <QApplication>
+extern int snippetMain(int, const char * const *);
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    return snippetMain(argc, argv);
 }
