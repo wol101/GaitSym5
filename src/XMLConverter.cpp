a26b83267d6a1d8f8106df708d77318de664e007
Bill Sellers <wis@mac.com>
2025-01-09 15:19:59 +0000

    Using the slower but likely more reliable string version of the python ApplyGenome

--- src/XMLConverter.cpp
+++ src/XMLConverter.cpp
@@ -106,6 +106,7 @@ void XMLConverter::GetFormattedXML(std::string *formattedXML)
 // the XML file specifying the simulation
 int XMLConverter::ApplyGenome(const std::vector<double> &genomeData)
 {
+    // slow but reliable version by creating a python command to assign the list
     std::vector<std::string> stringList;
     stringList.reserve(genomeData.size());
     for (auto &&x : genomeData) { stringList.push_back(GSUtil::ToString(x)); }
@@ -114,23 +115,26 @@ int XMLConverter::ApplyGenome(const std::vector<double> &genomeData)
     if (!ok) std::cerr << "Error in XMLConverter.cpp Line = " << __LINE__ << "\n";
     if (!ok) return __LINE__;
 
+    // faster but I am not sure this actually works
     // Create a list: [1, 2, 3]
-    py_Ref r0 = py_getreg(0);
-    py_newlistn(r0, int(genomeData.size()));
-    for (size_t i = 0; i < genomeData.size(); ++i) { py_newfloat(py_list_getitem(r0, int(i)), genomeData[i]); }
-    py_setglobal(py_name("gg"), r0);
+    // py_Ref r0 = py_getreg(0); // create on register
+    // py_push(r0); // save on stack because I don't know what might effect the registers
+    // py_Ref s0 = py_peek(-1); // get stack value
+    // py_newlistn(r0, int(genomeData.size()));
+    // for (size_t i = 0; i < genomeData.size(); ++i) { py_newfloat(py_list_getitem(s0, int(i)), genomeData[i]); }
+    // py_setglobal(py_name("gg"), s0);
 
-    // test
-    for (size_t i = 0; i < genomeData.size(); ++i)
-    {
-        std::string testString = "g["s + GSUtil::ToString(i) + "]==gg["s  + GSUtil::ToString(i) + "]"s;
-        ok = py_eval(m_smartSubstitutionParserText[i].c_str(), NULL);
-        if (!py_tobool(py_retval()))
-        {
-            if (!ok) std::cerr << "Error in XMLConverter.cpp Line = " << __LINE__ << "\n";
-            return __LINE__;
-        }
-    }
+    // // test
+    // for (size_t i = 0; i < genomeData.size(); ++i)
+    // {
+    //     std::string testString = "g["s + GSUtil::ToString(i) + "]==gg["s  + GSUtil::ToString(i) + "]"s;
+    //     ok = py_eval(m_smartSubstitutionParserText[i].c_str(), NULL);
+    //     if (!py_tobool(py_retval()))
+    //     {
+    //         if (!ok) std::cerr << "Error in XMLConverter.cpp Line = " << __LINE__ << "\n";
+    //         return __LINE__;
+    //     }
+    // }
 
     double v = 0;
     for (size_t i = 0; i < m_smartSubstitutionParserText.size(); i++)
