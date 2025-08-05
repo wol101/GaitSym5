b941a36c76df00b03b240cd8da29031a66bd3d9e
Bill Sellers <wis@mac.com>
2025-08-05 09:55:16 +0100

    Added a bit more detail to the clean script

--- clean.sh
+++ clean.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+echo "Delete build leftovers. Use -f to delete the CMakeLists.txt.user files."
 chmod +x clean.sh
 rm -rf build
 rm -rf GaitSymQt/build
 rm -rf terminal/build
 
 if [ "$1" = "-f" ]; then
-    rm GaitSymQt/CMakeLists.txt.user
+    rm -vf GaitSymQt/CMakeLists.txt.user
 fi
