952e1b374bf87b34f39405965b2eb02767282400
Bill Sellers <wis@mac.com>
2025-11-19 09:13:04 +0000

    Adding singularity files

---
+++ src/StringOfPearlsStrap.cpp
@@ -0,0 +1,18 @@
+/*
+ *  StringOfPearlsStrap.cpp
+ *  GaitSym5
+ *
+ *  Created by Bill Sellers on 31/10/2025.
+ *  Copyright 2005 Bill Sellers. All rights reserved.
+ *
+ */
+
+#include "StringOfPearlsStrap.h"
+
+using namespace std::string_literals;
+namespace GaitSym {
+
+
+StringOfPearlsStrap::StringOfPearlsStrap() {}
+
+}
