feb5c823c268e62860341a4fab07783c704520db
Bill Sellers <wis@mac.com>
2026-06-20 02:36:21 +0100

    Adding in the option to have zero length springs

--- src/DampedSpringMuscle.cpp
+++ src/DampedSpringMuscle.cpp
@@ -30,15 +30,18 @@ double DampedSpringMuscle::elasticEnergy()
     double delLen = strap()->length() - m_unloadedLength;
     if (delLen < 0) return 0;
 
-    // difference between these two values is the amount of energy lost by damping
-    // std::cerr << 0.5 * GetStrap()->GetTension() * delLen << "\n";
-    // std::cerr << 0.5 * m_SpringConstant * m_Area * delLen * delLen / m_UnloadedLength << "\n";
-
-    return 0.5 * m_springConstant * m_area * delLen * delLen / m_unloadedLength;
+    if (m_unloadedLength > 0) // use the Youngs Modulus formulation
+    {
+        return 0.5 * m_springConstant * m_area * delLen * delLen / m_unloadedLength;
+    }
+    else // use the raw spring constant
+    {
+        return 0.5 * m_springConstant * delLen * delLen;
+    }
 }
 
 
-// update the tension depending on length and velocity
+// update the tension depending on length and velocit
 // activation is used as a linear multiplier
 void DampedSpringMuscle::updateActivation()
 {
@@ -46,40 +49,68 @@ void DampedSpringMuscle::updateActivation()
     else
     { m_activation = dataSum(); }
 
-    // calculate strain
-    double elasticStrain = (strap()->length() - m_unloadedLength) / m_unloadedLength;
-
-    // calculate stress
-    double elasticStress = elasticStrain * m_springConstant;
     double tension;
-    if (elasticStress <= 0) // if not stretching the spring then set tension to zero
+    if (m_unloadedLength > 0) // use the Youngs Modulus formulation
     {
-        tension = 0;
+        // calculate strain
+        double elasticStrain = (strap()->length() - m_unloadedLength) / m_unloadedLength;
+
+        // calculate stress
+        double elasticStress = elasticStrain * m_springConstant;
+        if (elasticStress <= 0) // if not stretching the spring then set tension to zero
+        {
+            tension = 0;
+        }
+        else
+        {
+
+            // calculate damping (+ve when lengthening)
+            double relativeVelocity = strap()->velocity() / m_unloadedLength;
+            double dampingStress = relativeVelocity * m_damping;
+
+            // now calculate tension
+            // NB. tension is negative when muscle shortening
+            tension = (elasticStress + dampingStress) * m_area * m_activation;
+
+            // stop any pushing
+            if (tension < 0) tension = 0;
+        }
     }
-    else
+    else // use the raw spring constant
     {
+        // calculate extension
+        double extension = strap()->length() - m_unloadedLength;
 
-        // calculate damping (+ve when lengthening)
-        double relativeVelocity = strap()->velocity() / m_unloadedLength;
-        double dampingStress = relativeVelocity * m_damping;
+        // calculate force
+        double elasticForce = extension * m_springConstant;
+        if (elasticForce <= 0) // if not stretching the spring then set tension to zero
+        {
+            tension = 0;
+        }
+        else
+        {
 
-        // now calculate tension
-        // NB. tension is negative when muscle shortening
-        tension = (elasticStress + dampingStress) * m_area * m_activation;
+            // calculate damping (+ve when lengthening)
+            double dampingForce = strap()->velocity() * m_damping;
 
-        // stop any pushing
-        if (tension < 0) tension = 0;
+            // now calculate tension
+            // NB. tension is negative when muscle shortening
+            tension = (elasticForce + dampingForce) * m_activation;
+
+            // stop any pushing
+            if (tension < 0) tension = 0;
+        }
     }
     strap()->setTension(tension);
 }
 
 bool DampedSpringMuscle::shouldBreak()
 {
-    if (m_breakingStrain <= 0) return false;
+    if (m_breakingStrain <= 0 || m_unloadedLength <= 0) return false; // breaking only implmented for Young's Modulus based springs
     double elasticStrain = (strap()->length() - m_unloadedLength) / m_unloadedLength;
     if (elasticStrain > m_breakingStrain)
     {
-        std::cerr << "DampedSpringMuscle::ShouldBreak returns true\n";
+        // std::cerr << "DampedSpringMuscle::ShouldBreak returns true\n";
         return true;
     }
     return false;
@@ -104,17 +135,20 @@ std::string DampedSpringMuscle::dumpToString()
 
 std::string *DampedSpringMuscle::createFromAttributes()
 {
+    // note the formulation can use stress/strain or Force/distance depending on whether the unloaded length is zero (you cannot calculate strains for an unloaded length of zero)
+    // spring_constant [N/m] = (Youngs_modulus [Pa] * area [m2]) / length [m]
+    // damping_constant [N/ms] = stress_per_strain_rate [Pa/s] * area [m2] / length [m]
     if (Muscle::createFromAttributes()) return lastErrorPtr();
     std::string buf;
-    if (findAttribute("UnloadedLength"s, &buf) == nullptr) return lastErrorPtr();
+    if (findAttribute("UnloadedLength"s, &buf) == nullptr) return lastErrorPtr(); // If the unloaded length is zero, use the spring constant rather than the Young's Modulus formulation
     this->setUnloadedLength(GSUtil::toDouble(buf.c_str()));
-    if (findAttribute("SpringConstant"s, &buf) == nullptr) return lastErrorPtr();
-    this->setSpringConstant(GSUtil::toDouble(buf.c_str()));
-    if (findAttribute("Area"s, &buf) == nullptr) return lastErrorPtr();
+    if (findAttribute("SpringConstant"s, &buf) == nullptr) return lastErrorPtr(); // If the unloaded length is zero, this value is in N/m otherwise it is the Young's Modulus in Pa
+    this->setSpringConstant(GSUtil::toDouble(buf.c_str())); // If the unloaded length is zero, this value is in N/ms otherwise it is the stress for unit rate of strain in Pa/s
+    if (findAttribute("Area"s, &buf) == nullptr) return lastErrorPtr(); // If the unloaded length is zero, this value is only used for visualisation
     this->setArea(GSUtil::toDouble(buf.c_str()));
     if (findAttribute("DampingConstant"s, &buf) == nullptr) return lastErrorPtr();
     this->setDamping(GSUtil::toDouble(buf.c_str()));
-    if (findAttribute("BreakingStrain"s, &buf) == nullptr) return lastErrorPtr();
+    if (findAttribute("BreakingStrain"s, &buf) == nullptr) return lastErrorPtr(); // If the unloaded length is zero, this value is not used
     this->setBreakingStrain(GSUtil::toDouble(buf.c_str()));
     return nullptr;
 }
